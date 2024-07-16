053980cc30f6c777fb4c7e048c6453c46099b93b
liuzy8 <liuzy8@ziroom.com>
2024-07-16 19:37:53 +0800

    feat: 地址页

--- src/pages/address/index.vue
+++ src/pages/address/index.vue
@@ -44,6 +44,15 @@
           placeholder="请输入备注信息"
         />
       </view>
+      <checkbox-group @change="handleCheckChange" class="check-box">
+        <label>
+          <checkbox
+            :checked="radioCheck"
+            color="#66BB6A"
+            style="transform: scale(0.7)"
+          />当前订单信息为下单后联系渠道，同意后方可提交订单
+        </label>
+      </checkbox-group>
       <view class="uni-btn-v">
         <button form-type="submit">保存/修改</button>
       </view>
@@ -55,6 +64,7 @@
 import { ref, computed } from "vue";
 import { CommonModule } from "@/store";
 import { updateUserInfo, getUserInfoToOpenId } from "@/api";
+const radioCheck = ref(false);
 const userId = computed(() => CommonModule.state.userId);
 const openId = computed(() => CommonModule.state.openId);
 const phone = computed(() => CommonModule.state.phone);
@@ -62,6 +72,14 @@ const name = computed(() => CommonModule.state.name);
 const address = computed(() => CommonModule.state.address);
 const remake = computed(() => CommonModule.state.remake);
 const formSubmit = async (e) => {
+  if (!radioCheck.value) {
+    uni.showToast({
+      icon: "none",
+      mask: true,
+      title: `请授权同意您的信息`,
+    });
+    return;
+  }
   console.log(
     "form发生了submit事件，携带数据为：" + JSON.stringify(e.detail.value)
   );
@@ -126,6 +144,9 @@ const formSubmit = async (e) => {
   } catch {
     uni.hideLoading();
   }
+};
+const handleCheckChange = () => {
+  radioCheck.value = !radioCheck.value;
 };
 </script>
 <style lang="scss" scoped>
